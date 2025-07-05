4a00a11374adcce90d668b509de357de9348ccf6
Kovvada Naga Venkata Sai Meghana <139636360+SaiMeghana14@users.noreply.github.com>
2025-07-05 19:10:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,9 +14,19 @@ st.title("🧠 MoodMate – Your Mood Companion")
 # -------------------------------
 # 🔐 Firebase Initialization
 # -------------------------------
-import json
-firebase_config = json.loads(st.secrets["firebase"].to_json())
-
+firebase_config = {
+    "type": st.secrets["firebase"]["type"],
+    "project_id": st.secrets["firebase"]["project_id"],
+    "private_key_id": st.secrets["firebase"]["private_key_id"],
+    "private_key": st.secrets["firebase"]["private_key"].replace("\\n", "\n"),
+    "client_email": st.secrets["firebase"]["client_email"],
+    "client_id": st.secrets["firebase"]["client_id"],
+    "auth_uri": st.secrets["firebase"]["auth_uri"],
+    "token_uri": st.secrets["firebase"]["token_uri"],
+    "auth_provider_x509_cert_url": st.secrets["firebase"]["auth_provider_x509_cert_url"],
+    "client_x509_cert_url": st.secrets["firebase"]["client_x509_cert_url"],
+    "universe_domain": st.secrets["firebase"]["universe_domain"]
+}
 if not firebase_admin._apps:
     cred = credentials.Certificate(firebase_config)
     firebase_admin.initialize_app(cred)
