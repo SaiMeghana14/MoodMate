7d2086d75c746ff6184aea8a5f79ea0b897f7d75
Kovvada Naga Venkata Sai Meghana <139636360+SaiMeghana14@users.noreply.github.com>
2025-07-05 17:45:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,22 +11,7 @@ st.set_page_config(page_title="MoodMate", page_icon="🧠")
 # ---------------------------------------
 # 🔐 Firebase Initialization from secrets
 # ---------------------------------------
-firebase_config = st.secrets["firebase"]
-
-cred = credentials.Certificate({
-    "type": firebase_config["type"],
-    "project_id": firebase_config["project_id"],
-    "private_key_id": firebase_config["private_key_id"],
-    "private_key": firebase_config["private_key"],
-    "client_email": firebase_config["client_email"],
-    "client_id": firebase_config["client_id"],
-    "auth_uri": firebase_config["auth_uri"],
-    "token_uri": firebase_config["token_uri"],
-    "auth_provider_x509_cert_url": firebase_config["auth_provider_x509_cert_url"],
-    "client_x509_cert_url": firebase_config["client_x509_cert_url"],
-    "universe_domain": firebase_config["universe_domain"]
-})
-
+cred = credentials.Certificate(st.secrets["firebase"])
 if not firebase_admin._apps:
     firebase_admin.initialize_app(cred)
 
