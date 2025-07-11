6a5034e14b62787db49e90cca3723871d7659c51
Kovvada Naga Venkata Sai Meghana <139636360+SaiMeghana14@users.noreply.github.com>
2025-07-11 16:19:22 +0530

    Update achievements.py

--- utils/achievements.py
+++ utils/achievements.py
@@ -2,7 +2,7 @@ import streamlit as st
 from utils.auth import load_users, save_users
 import datetime
 
-def achievements_page():
+def achievements_page(user):
     st.subheader("🏅 Your Achievements")
     if "user" not in st.session_state:
         st.warning("Please login to see your streaks.")
@@ -10,6 +10,9 @@ def achievements_page():
 
     users = load_users()
     user = st.session_state.user
+    if user not in users:
+        st.warning(f"No data found for user **{user}**.")
+        return
     journals = users[user].get("journals", {})
 
     today = datetime.date.today()
